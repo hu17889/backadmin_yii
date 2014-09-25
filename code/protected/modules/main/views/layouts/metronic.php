213ef3a65f65b952b62b085dc482844a72d6c547
hucong <hucong@backadmin.hucong.net>
2014-09-24 23:09:40 -0400

    change

--- code/protected/modules/main/views/layouts/metronic.php
+++ code/protected/modules/main/views/layouts/metronic.php
@@ -4,7 +4,7 @@
 <!--[if !IE]><!--> <html lang="ch"> <!--<![endif]-->
 <head>
    <meta charset="utf-8" />
-   <title><?php echo htmlspecialchars($this->actionName);?>-推荐系统后台</title>
+   <title><?php echo htmlspecialchars($this->actionName);?>-backadmin</title>
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
    <meta content="" name="description" />
@@ -120,7 +120,7 @@
 <!-- BEGIN FOOTER -->
 <div class="footer">
     <div class="footer-inner">
-        2014 &copy; Back Admin by hucong.
+        2014 &copy; Back Admin by <a target="_blank" href="http://hucong.net">胡户主</a>.
     </div>
     <div class="footer-tools">
         <span class="go-top">
--- code/protected/modules/main/views/user/login_soft.php
+++ code/protected/modules/main/views/user/login_soft.php
@@ -12,7 +12,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 <!-- BEGIN HEAD -->
 <head>
 	<meta charset="utf-8" />
-	<title>Metronic | Login Options - Login Form 2</title>
+	<title>backadmin后台登录</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 	<meta content="" name="description" />
@@ -208,7 +208,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 	<!-- END LOGIN -->
 	<!-- BEGIN COPYRIGHT -->
 	<div class="copyright">
-		2014 &copy;  by 美股时代.
+		2014 &copy; Back Admin by <a target="_blank" href="http://hucong.net">胡户主</a>.
 	</div>
 	<!-- END COPYRIGHT -->
 	<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
